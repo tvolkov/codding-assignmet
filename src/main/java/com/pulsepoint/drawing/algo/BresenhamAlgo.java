97355d3e435c63afc05c58a52165e9b5906ec499
Timofey Volkov <timofey.volkov@tiger.trade>
2022-02-18 18:01:11 +0100

    fix bresenham low

--- src/main/java/com/pulsepoint/drawing/algo/BresenhamAlgo.java
+++ src/main/java/com/pulsepoint/drawing/algo/BresenhamAlgo.java
@@ -6,6 +6,7 @@ import com.pulsepoint.drawing.primitive.Colour;
 import com.pulsepoint.drawing.primitive.Pixel;
 import com.pulsepoint.drawing.primitive.Point;
 import com.pulsepoint.drawing.primitive.Shape;
+
 import java.util.Iterator;
 
 /**
@@ -18,91 +19,93 @@ import java.util.Iterator;
  */
 public class BresenhamAlgo {
 
-  public Shape drawLine(Point start, Point end, Colour colour) {
-    if (abs(start.getY() - end.getY()) > (start.getX() - end.getX())) {
-      if (start.getX() > end.getX()) {
-        return bresenhamLow(end.getX(), end.getY(), start.getX(), start.getY(), colour);
-      } else {
-        return bresenhamLow(start.getX(), start.getY(), end.getX(), end.getY(), colour);
-      }
-    } else {
-      if (start.getY() > end.getY()) {
-        return bresenhamHeight(end.getX(), end.getY(), start.getX(), start.getY(), colour);
-      } else {
-        return bresenhamHeight(start.getX(), start.getY(), end.getX(), end.getY(), colour);
-      }
+    public Shape drawLine(Point start, Point end, Colour colour) {
+        if (abs(start.getY() - end.getY()) > (start.getX() - end.getX())) {
+            if (start.getX() > end.getX()) {
+                return bresenhamLow(end.getX(), end.getY(), start.getX(), start.getY(), colour);
+            } else {
+                return bresenhamLow(start.getX(), start.getY(), end.getX(), end.getY(), colour);
+            }
+        } else {
+            if (start.getY() > end.getY()) {
+                return bresenhamHeight(end.getX(), end.getY(), start.getX(), start.getY(), colour);
+            } else {
+                return bresenhamHeight(start.getX(), start.getY(), end.getX(), end.getY(), colour);
+            }
+        }
     }
-  }
 
-  private Shape bresenhamLow(int x0, int y0, int x1, int y1, Colour colour) {
-    final int deltaX = abs(x1 - x0);
-    final int deltaY = y1 - y0;
+    private Shape bresenhamLow(int x0, int y0, int x1, int y1, Colour colour) {
+        final int deltaX = abs(x1 - x0);
+        final int deltaY = y1 - y0;
 
-    final int directionY = deltaY > 0 ? 1 : -1;
+        final int directionY = deltaY > 0 ? 1 : -1;
 
-    final int deltaError = abs(deltaY);
+        final int deltaError = abs(deltaY);
 
-    return new Shape(
-        () ->
-            new Iterator<>() {
-              int currentX = x0;
-              int currentY = y0;
-              int error = 0;
+        return new Shape(
+                () ->
+                        new Iterator<>() {
+                            int currentX = x0;
+                            int currentY = y0;
+                            int error = 0;
 
-              @Override
-              public boolean hasNext() {
-                return currentX <= x1;
-              }
+                            @Override
+                            public boolean hasNext() {
+                                return currentX <= x1 && currentY <= y1;
+                            }
 
-              @Override
-              public Pixel next() {
-                Pixel result = new Pixel(currentX, currentY, colour);
+                            @Override
+                            public Pixel next() {
+                                Pixel result = new Pixel(currentX, currentY, colour);
 
-                error = error + deltaError;
-                if (2 * error > deltaX) {
-                  currentY = currentY + directionY;
-                  error = error - deltaX;
-                }
+                                error = error + deltaError;
+                                if (2 * error > deltaX) {
+                                    currentY = currentY + directionY;
+                                    error = error - deltaError;
+                                }
 
-                currentX++;
-                return result;
-              }
-            });
-  }
+                                if (error > deltaY) {
+                                    currentX++;
+                                }
+                                return result;
+                            }
+                        });
+    }
 
-  private Shape bresenhamHeight(int x0, int y0, int x1, int y1, Colour colour) {
-    final int deltaX = x1 - x0;
-    final int deltaY = abs(y1 - y0);
+    private Shape bresenhamHeight(int x0, int y0, int x1, int y1, Colour colour) {
+        final int deltaX = x1 - x0;
+        final int deltaY = abs(y1 - y0);
 
-    final int directionX = deltaX > 0 ? 1 : -1;
+        final int directionX = deltaX > 0 ? 1 : -1;
 
-    final int deltaError = abs(deltaX);
+        final int deltaError = abs(deltaX);
 
-    return new Shape(
-        () ->
-            new Iterator<>() {
-              int currentX = x0;
-              int currentY = y0;
-              int error = 0;
+        return new Shape(
+                () ->
+                        new Iterator<>() {
+                            int currentX = x0;
+                            int currentY = y0;
+                            int error = 0;
 
-              @Override
-              public boolean hasNext() {
-                return currentY <= y1;
-              }
+                            @Override
+                            public boolean hasNext() {
+                                return currentY <= y1;
+                            }
 
-              @Override
-              public Pixel next() {
-                Pixel result = new Pixel(currentX, currentY, colour);
+                            @Override
+                            public Pixel next() {
+                                Pixel result = new Pixel(currentX, currentY, colour);
 
-                error = error + deltaError;
-                if (2 * error > deltaY) {
-                  currentX = currentX + directionX;
-                  error = error - deltaY;
-                }
+                                error = error + deltaError;
+                                if (2 * error > deltaY) {
+                                    currentX = currentX + directionX;
+                                    error = error - deltaY;
+                                }
 
-                currentY++;
-                return result;
-              }
-            });
-  }
+                                currentY++;
+                                return result;
+                            }
+                        });
+    }
 }
